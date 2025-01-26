a8198d9b4aba0f1d5c554c835d4300366becc19d
Meti <silvie8202@gmail.com>
2025-01-26 10:32:02 +0300

    feat: Define Post and User models

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)  # Plain text (vulnerable)
